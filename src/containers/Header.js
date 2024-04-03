b5f020fac61cf06db9a0b44dd3df54d93f576a43
Acer <pramodparam24@gmail.com>
2024-04-03 15:01:10 +0530

    first commit

---
+++ src/containers/Header.js
@@ -0,0 +1,20 @@
+
+import React from "react";
+
+const Header = () => {
+    return (<div className="ui fixed menu ">
+        <div className="ui container center">
+            <h1>Redux Shop</h1>
+
+            
+            
+            {/* <button type="button" style={{borderRadius:"5px",fontSize:"20px"}} class="btn btn-dark button">Login</button>
+            <button type="button" style={{borderRadius:"5px",fontSize:"20px",marginLeft:'10px'}}>Register</button> */}
+            
+        </div>
+
+
+    </div>  );
+}
+ 
+export default Header;
